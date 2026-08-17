1e7c3a7d6b64df53d7d8525566c8afa4b2dd5a66
codethelabs <codethelabs@gmail.com>
2026-08-17 12:26:25 +0300

    feat(web): enable gzip compression and static assets cache-control in Nginx serving

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Nisoko Deployment Shortcut
+# Run this script to deploy full changes
+nisoko deploy --app reos-frontend-rswhm
---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Nisoko Push Shortcut
+# Run this script to push incremental changes
+nisoko push --app reos-frontend-rswhm
